523e0925578697638f59bed0bf4bc501c569f949
Nate Denys <nate_denys@yahoo.com>
2017-08-31 13:07:53 -0700

    Implement Trash button aka delete timer functionality

--- public/app.js
+++ public/app.js
@@ -26,6 +26,9 @@ const TimersDashboard = React.createClass({
 	handleEditFormSubmit: function (attrs) {
 		this.updateTimer(attrs);
 	},
+	handleTrashClick: function (timerId) {
+		this.deleteTimer(timerId);
+	},
 	createTimer: function (timer) {
 		const t = helpers.newTimer(timer);
 		this.setState({
@@ -46,6 +49,11 @@ const TimersDashboard = React.createClass({
 			}),
 		});
 	},
+	deleteTimer: function (timerId) {
+		this.setState({
+			timers: this.state.timers.filter(t => t.id !== timerId),
+		});
+	},
 	render: function () {
 		return (
 			<div className='ui three column centered grid'>
@@ -53,6 +61,7 @@ const TimersDashboard = React.createClass({
 					<EditableTimerList 
 						timers={this.state.timers}
 						onFormSubmit={this.handleEditFormSubmit}
+						onTrashClick={this.handleTrashClick}
 					/>
 					<ToggleableTimerForm
 						onFormSubmit={this.handleCreateFormSubmit}
@@ -74,6 +83,7 @@ const EditableTimerList = React.createClass({
 				elapsed={timer.elapsed}
 				runningSince={timer.runningSince}
 				onFormSubmit={this.props.onFormSubmit}
+				onTrashClick={this.props.onTrashClick}
 			/>
 		));
 		return (
@@ -126,6 +136,7 @@ const EditableTimer = React.createClass({
 					elapsed={this.props.elapsed}
 					runningSince={this.props.runningSince}
 					onEditClick={this.handleEditClick}
+					onTrashClick={this.props.onTrashClick}
 				/>
 			);
 		}
@@ -220,6 +231,9 @@ const ToggleableTimerForm = React.createClass({
 });
 
 const Timer = React.createClass({
+	handleTrashClick: function () {
+		this.props.onTrashClick(this.props.id);
+	},
 	render: function () {
 		const elapsedString = helpers.renderElapsedString(this.props.elapsed);
 		return (
@@ -243,7 +257,10 @@ const Timer = React.createClass({
 						>
 							<i className='edit icon'></i>
 						</span>
-						<span className='right floated trash icon'>
+						<span 
+							className='right floated trash icon'
+							onClick={this.handleTrashClick}
+						>
 							<i className='trash icon'></i>
 						</span>
 					</div>
